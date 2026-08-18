995e019b0445a08d3494744a9c5ece8903934022
mohd-afeef-badri <afeef.badri@gmail.com>
2026-08-18 18:06:08 +0200

    add other versions of psd and gmsh as dependency

--- spack_repo/psd/packages/psd/package.py
+++ spack_repo/psd/packages/psd/package.py
@@ -16,15 +16,21 @@ class Psd(AutotoolsPackage):
     & dynamic  regimes, with support for linear and nonlinear models 
     """
     homepage = "https://mohd-afeef-badri.github.io/psd"
-    url = "https://github.com/mohd-afeef-badri/psd/archive/0a67ccbdf2585cb8250ac3c935ae9a27b533f822.tar.gz"
+    url = "https://github.com/mohd-afeef-badri/psd/archive/refs/tags/v2.6.tar.gz"
     git = "https://github.com/mohd-afeef-badri/psd.git"
 
     maintainers("mohd-afeef-badri")
 
     license("Apache-2.0", checked_by="mohd-afeef-badri")
 
-#    version("main", commit="56267508db3b209a487c0d217116ca5b841f91ce")
     version("main", branch="main")
+    version("2.6", sha256="5716e91e7fc79ffd125976fba0db9373b2e01a00883fb17e6b033d077fbf1888")
+    version("2.5", sha256="16ec15f5baf78e1d9466db78ae4f34a160d585bc11665c835c829809ecc6e657")
+    version("2.4", sha256="ba7916bc844ab635fda2b6fdae956d2f27baeb44669f3c2a5280b81dec5339b6")
+    version("2.3", sha256="b82fd1beb99c65b5a64f60370cdff78b9b2c7466366168e1d6da6c3db025a3af")
+    version("2.2", sha256="f3b1b258976ea69be825ed64004acda7d3a865da003ecd4c7dfa1e38f6a5391c")
+    version("2.1", sha256="24e905361c821b7789f77c6ec8012cb2ae36903e421f1a76efba3f05ca32c777")
+    version("2.0", sha256="d59fce3add565f3b9b0e56723c31ed06016704ca6f0af108f30e6ef1d2fde10c")
 
     depends_on("cxx", type="build")
 
@@ -32,6 +38,7 @@ class Psd(AutotoolsPackage):
     depends_on("automake", type="build")
     depends_on("libtool", type="build")
     depends_on("m4", type="build")
+    depends_on("gmsh", type=("build", "run"))
 
     depends_on("tfel")
     depends_on("mgis")
